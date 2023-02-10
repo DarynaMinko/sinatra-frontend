22527b27803d35a4c91a2d95db1718293279bf93
rafiki <zaneobwoni@gmail.com>
2023-02-10 20:39:49 +0100

    fixed prettier for whole app

--- apps/main/src/routes/layout.tsx
+++ apps/main/src/routes/layout.tsx
@@ -1,30 +1,30 @@
-import { component$, Slot } from '@builder.io/qwik';
-import { loader$ } from '@builder.io/qwik-city';
+import { component$, Slot } from "@builder.io/qwik";
+import { loader$ } from "@builder.io/qwik-city";
 
-import Header from '../components/header/header';
+import Header from "../components/header/header";
 
 export const serverTimeLoader = loader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
+    return {
+        date: new Date().toISOString()
+    };
 });
 
 export default component$(() => {
-  const serverTime = serverTimeLoader.use();
-  return (
-    <>
-      <main>
-        <Header />
-        <section>
-          <Slot />
-        </section>
-      </main>
-      <footer>
-        <a href="https://www.builder.io/" target="_blank">
-          Made with ♡ by Builder.io
-          <div>{serverTime.value.date}</div>
-        </a>
-      </footer>
-    </>
-  );
+    const serverTime = serverTimeLoader.use();
+    return (
+        <>
+            <main>
+                <Header />
+                <section>
+                    <Slot />
+                </section>
+            </main>
+            <footer>
+                <a href="https://www.builder.io/" target="_blank">
+                    Made with ♡ by Builder.io
+                    <div>{serverTime.value.date}</div>
+                </a>
+            </footer>
+        </>
+    );
 });
