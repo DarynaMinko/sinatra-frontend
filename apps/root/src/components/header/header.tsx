72fbb20b9a2bb0c11e45d4200328fab7104d88ec
rafiki <zaneobwoni@gmail.com>
2023-02-22 20:41:45 +0100

    removed app/docs folder

--- apps/root/src/components/header/header.tsx
+++ apps/root/src/components/header/header.tsx
@@ -3,8 +3,10 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
     return (
         <header>
-            <nav class={`container mx-auto flex bg-green-300 justify-between`}>
-                <div class={`flex`}>
+            <nav
+                class={`container mx-auto flex bg-green-300 justify-between min-h-[40px] items-center`}
+            >
+                <div class={`flex space-x-6`}>
                     <div>back | forward</div>
                     <input type="text" class={`bg-red-300 rounded-2xl`} />
                 </div>
