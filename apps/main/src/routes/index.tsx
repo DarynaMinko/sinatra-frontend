be51190e9975adb9cbb7b3b3345de1431c23f46b
rafiki <zaneobwoni@gmail.com>
2023-02-22 14:38:45 +0100

    updated to use tailwind

--- apps/main/src/routes/index.tsx
+++ apps/main/src/routes/index.tsx
@@ -4,7 +4,7 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
     return (
         <div>
-            <h1>Welcome to Qwik</h1>
+            <h1 class="text-red-500">Welcome to Qwik</h1>
             <p>thisi is a music app</p>
         </div>
     );
