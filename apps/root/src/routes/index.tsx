8c382a5a0a5b8402ded3c5fc4ad2ec6ef78512cf
rafiki <zaneobwoni@gmail.com>
2023-02-22 21:21:08 +0100

    added button components

--- apps/root/src/routes/index.tsx
+++ apps/root/src/routes/index.tsx
@@ -1,11 +1,14 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Button } from "~/components/shared/button";
 
 export default component$(() => {
     return (
         <div class={`container mx-auto`}>
             <h1 class="text-red-500 text-">What's hot this weekend</h1>
             <p>thisi is a music app</p>
+            <Button>primary</Button>
+            <Button intent="secondary">Secondary</Button>
         </div>
     );
 });
--- apps/root/src/routes/layout.tsx
+++ apps/root/src/routes/layout.tsx
@@ -1,8 +1,6 @@
 import { component$, Slot } from "@builder.io/qwik";
 import { loader$ } from "@builder.io/qwik-city";
 
-import Header from "../components/header/header";
-
 export const serverTimeLoader = loader$(() => {
     return {
         date: new Date().toISOString()
@@ -10,18 +8,30 @@ export const serverTimeLoader = loader$(() => {
 });
 
 export default component$(() => {
-
     return (
         <>
             <main>
-                <Header />
+                <div
+                    class={`container mx-auto flex bg-green-300 justify-between min-h-[40px] items-center`}
+                >
+                    <div class={`flex space-x-6`}>
+                        <div>back | forward</div>
+                        <input type="text" class={`bg-red-300 rounded-2xl`} />
+                    </div>
+                    <div>
+                        <a class={`text-green-600`} href="/">
+                            Home
+                        </a>
+                        <a class={`text-green-600`} href="/about">
+                            About
+                        </a>
+                    </div>
+                </div>
                 <section>
                     <Slot />
                 </section>
             </main>
-            <footer>
-                this is a footer
-            </footer>
+            <footer>this is a footer</footer>
         </>
     );
 });
