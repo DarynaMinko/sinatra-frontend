3cd0dc8e4e79914ddd4be0137bd454941b455079
DarynaMinko <106735374+DarynaMinko@users.noreply.github.com>
2023-03-20 16:08:42 +0100

    prelude before ui

--- apps/prelude/app/page.tsx
+++ apps/prelude/app/page.tsx
@@ -1,12 +1,18 @@
-import { Button } from "ui/Button";
-import "../styles/globals.css"
+'use client';
+import  Button  from "ui/Button";
+import "../styles/globals.css";
+
 
 export default function Web() {
+    // const handleClick = () => {
+    //     console.log('Clicked!');
+    //   };
+   
     return (
         <div>
-            <h1>Web</h1>
-            <h1>This is index page example</h1>
-            <Button>This is a Web!</Button>
+            <div className="text-2xl bg-teal-500">alksjdflk</div>
+             <Button  onClick={() => console.log("You clicked!")}> INLOGGEN </Button>
+             <Button intent="secondary" size='small'  onClick={() => console.log("You clicked!")}> OUTLoggen </Button>
         </div>
     );
 }
--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,5 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
-    content: ["./**/*.{html,ts,tsx,js,jsx}"],
-    theme: {},
-    plugins: []
-};
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
