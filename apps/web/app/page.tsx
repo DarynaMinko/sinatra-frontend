637e4a5aabf0c0fd7e8b84095c1993927db040f3
zaneObwoni <zaneobwoni@gmail.com>
2023-04-18 17:40:42 +0200

    fixed ui issue

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import "../styles/globals.css";
+import { Button } from "./components/Button";
+
+export default function Web() {
+    // const handleClick = () => {
+    //     console.log('Clicked!');
+    //   };
+
+    return (
+        <div>
+            <Button onClick={() => console.log("You clicked!")}>
+                {" "}
+                INLOGGEN{" "}
+            </Button>
+            <Button
+                intent="secondary"
+                size="small"
+                onClick={() => console.log("You clicked!")}
+            >
+                {" "}
+                REGISTERED{" "}
+            </Button>
+            <Button
+                intent="primary_header"
+                size="medium"
+                onClick={() => console.log("You clicked!")}
+            >
+                {" "}
+                ASK YOU QUESTION HERE{" "}
+            </Button>
+            <Button
+                intent="secondary"
+                size="medium"
+                onClick={() => console.log("You clicked!")}
+            >
+                {" "}
+                VIEW OUR OFFER{" "}
+            </Button>
+        </div>
+    );
+}
